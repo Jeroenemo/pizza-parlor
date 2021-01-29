efa229055678ee9bafed08f14d1d6fe3115e95bb
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-29 08:27:54 -0800

    add spec for default price

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-Describe: Pizza();
-Test: "It should create an object with the passed in arguments for parameters topping and size."
-Expect(pizza = new Pizza("cheese", "large").toEqual(Pizza {topping: "cheese", size: "large"}));
+// Describe: Pizza();
+// Test: "It should create an object with the passed in arguments for parameters topping and size."
+// Expect(pizza = new Pizza("cheese", "large").toEqual(Pizza {topping: "cheese", size: "large"}));
+
+// Test: "It should instantiate new pizza objects with default price of 0"
+// Expect(pizza = new Pizza("cheese", "large").toEqual(Pizza {topping: "cheese", size: "large", price: 0}));
 
 function Pizza(topping, size) {
   this.topping = topping;
   this.size = size;
+  this.price = 0;
 }
