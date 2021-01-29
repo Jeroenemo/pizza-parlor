a8c822a3cfc204cb944e1c1f88b8a1dfb69a54d2
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-29 10:22:10 -0800

    add assignId proto to assign id's to order values

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,41 @@
 // Expect(pizza = new Pizza("large").toEqual(Pizza {toppings: [], size: "large", price: 0}));
 
 // Test: "It should instantiate new pizza objects with "undefined" for size, as to set it with prototype later"
-// Expect(pizza = new Pizza().toEqual(Pizza {toppings: [], size: undefined, price: 0}))
+// Expect(pizza = new Pizza().toEqual(Pizza {toppings: [], size: undefined, price: 0}));
 
 // Describe: Pizza.prototype.addTopping();
 // Test: "It should push topping into pizza object"
-// Expect(pizza.addTopping("cheese").toEqual(Pizza {toppings: ["cheese"], size: undefined, price: 0}))
+// Expect(pizza.addTopping("cheese").toEqual(Pizza {toppings: ["cheese"], size: undefined, price: 0}));
 
 // Describe: Pizza.prototype.addSize();
 // Test: "It should set size variable of pizza object"
-// Expect(pizza.addSize("large").toEqual(Pizza {toppings: [], size: "large", price: 0}))
+// Expect(pizza.addSize("large").toEqual(Pizza {toppings: [], size: "large", price: 0}));
 
 // Describe: Pizza.prototype.calculateCost();
 // Test: "It should add to this.price of pizza object depending on size."
 // Test: "This test assumes pre-existing pizza object has size: "medium"
-// Expect(pizza.calculateCost().toEqual(Pizza {toppings: [], size: "medium", price: 6}))
+// Expect(pizza.calculateCost().toEqual(Pizza {toppings: [], size: "medium", price: 6}));
 
 // Test: "It should add 3 to this.price for each element in this.toppings array"
 // Test: "This test assumes this.toppings are ["cheese", "pepperoni"], and size is "medium"
-// Expect(pizza.calculateCost().toEqual(Pizza {toppings: ["cheese", "pepperoni"], size: "medium", price: 12})) 
+// Expect(pizza.calculateCost().toEqual(Pizza {toppings: ["cheese", "pepperoni"], size: "medium", price: 12})) ;
 
 // Describe Order();
 // Test: "It should instantiate Order objects with empty {} object "
-// Expect(order = new Order().toEqual(Order {order: {}}))
+// Expect(order = new Order().toEqual(Order {order: {}}));
+
+// Describe Order.prototype.assignId();
+// Test: "It should add current id to object, incrementing by 1."
+// Expect(order.assignId().toEqual(Order {order: {}, currentId: 1}));
+
 function Order() {
   this.order = {};
+  this.currentId = 0;
+}
+
+Order.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId
 }
 
 function Pizza() {
