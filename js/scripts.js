c486d8eaf21ec659f2aa113f0c8b5bce283c129f
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-04 17:02:36 -0800

    add switch statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function Order() {
   this.pizzas = {};
   this.total = 0;
-  this.currentId = 1;
+  this.currentId = 0;
 };
 Order.prototype.assignId = function() {
   this.currentId += 1;
@@ -28,33 +28,38 @@ function Pizza() {
   this.size = undefined;
   this.sauce = undefined;
 };
-Pizza.prototype.addTopping = function(topping) {
-  topping = topping.replace(/\s+/g, '')
-  if (topping !== "") {
-    this.toppings.push(topping);
-  };  
-};
+// Pizza.prototype.addTopping = function(topping) {
+//   topping = topping.replace(/\s+/g, '')
+//   if (topping !== "") {
+//     this.toppings.push(topping);
+//   };  
+// };
 Pizza.prototype.addSize = function(size) {
   this.size = size;
 };
 Pizza.prototype.addSauce = function(sauce) {
   this.sauce = sauce;
-}
+};
 Pizza.prototype.calculatePrice = function() {
-  if (this.size === "Small") {
-    this.price += 4;
-  } else if (this.size === "Medium") {
-    this.price += 6;
-  } else {
-    this.price += 8;
-  };
-  if (this.sauce === "White") {
-    this.price += 1;
-  } else if (this.sauce === "Secret") {
-    this.price += 2;
-  };
+  switch(true) {
+    case (this.size === "Small"):
+      this.price += 4;
+      break;
+    case (this.size === "Medium"):
+      this.price += 6;
+      break;
+    case (this.size === "Large"):
+      this.price += 8;
+      break;
+    case (this.sauce === "White"):
+      this.price += 1;
+      break;
+    case (this.sauce === "Secret"):
+      this.price += 2;
+      break;
+  }
   for (i = 0; i < this.toppings.length; i ++) {
-    this.price += 3;
+    this.price += 2;
   };
 };
 // UI LOGIC
@@ -66,12 +71,12 @@ function displayOrder(ordertoDisplay) {
   Object.keys(order.pizzas).forEach(function(key) {
     const order = ordertoDisplay.getPizza(key);
     if (order.toppings.length === 0) {
-      htmlForOrder += "<li>" + "One " + order.size + " cheese pizza with " + order.sauce + " Sauce and NO toppings..." + "</li>" 
+      htmlForOrder += "<li>One " + order.size + " cheese pizza with " + order.sauce + " Sauce and NO toppings..." + "</li>" 
     } else {
-      htmlForOrder += "<li>One " + order.size + " pizza with " + order.toppings.join(", ") + " and " + order.sauce + " Sauce</li>"
-    }
+      htmlForOrder += "<li>One " + order.size + " pizza with " + order.sauce + " Sauce, " +order.toppings.join(", ").replace(/,(?=[^,]*$)/, ' and') + "</li>"
+    };
   });
-  htmlForOrder += "<br><h3> Your total is: $" +ordertoDisplay.total+ "</h3>"
+  htmlForOrder += "<br><h3> Your total is: $" + ordertoDisplay.total + "</h3>"
   orderList.html(htmlForOrder);
 };
 $(document).ready(function() {
@@ -83,13 +88,13 @@ $(document).ready(function() {
     let pizza = new Pizza();
     pizza.addSize($("input:radio[name=size]:checked").val());
     pizza.addSauce($("input:radio[name=sauce]:checked").val());
-    $("#shopping-cart").text("Number of items in your cart: " + order.currentId);
     $('#toppings input:checked').each(function() {
       pizza.toppings.push($(this).val());
       $("#input").trigger("reset");
-      });
-      pizza.calculatePrice();
-      order.addPizza(pizza);
+    });
+    pizza.calculatePrice();
+    order.addPizza(pizza);
+    $("#shopping-cart").text("Number of items in your cart: " + pizza.id);
     } else if (this.id === "complete-order") {
       $("div#order-input").hide();
       $("div#order-output").show();
@@ -97,6 +102,6 @@ $(document).ready(function() {
       displayOrder(order);
     } else if (this.id === "reset-page") {
       location.reload();
-    }
+    };
   });
-})
+});
